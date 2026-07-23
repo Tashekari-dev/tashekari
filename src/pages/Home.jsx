ae5227fd841c7a1dd74b77b016e748c366e81cd5
Tashekari-dev <tashekari07@gmail.com>
2026-07-24 05:18:39 +0530

    Auto deploy final test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,22 +11,32 @@ import WhyChooseUs from "../components/home/WhyChooseUs";
 import About from "../components/home/About";
 import BrandStory from "../components/home/BrandStory";
 import Newsletter from "../components/home/Newsletter";
+
 export default function Home() {
   return (
     <>
       <PageTransition>
-      <Navbar />
-      <Hero />
-      <BrandStory />
-      <VideoBanner />
-      <Testimonials />
-      <Categories />
-      <FeaturedProducts />
-      <WhyChooseUs />
-      <About />
-      <Newsletter />
-       <InstagramGallery />
-      <Footer />
+        <Navbar />
+
+        <Hero />
+
+        {/* TEMPORARY FINAL AUTO-DEPLOY TEST */}
+        <div className="border-y border-green-300 bg-green-100 px-4 py-3 text-center">
+          <p className="text-sm font-semibold text-green-800">
+            ✅ Auto Deploy Final Test
+          </p>
+        </div>
+
+        <BrandStory />
+        <VideoBanner />
+        <Testimonials />
+        <Categories />
+        <FeaturedProducts />
+        <WhyChooseUs />
+        <About />
+        <Newsletter />
+        <InstagramGallery />
+        <Footer />
       </PageTransition>
     </>
   );
